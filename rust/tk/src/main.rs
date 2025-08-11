7809d4a5817792c8e963ad957dc5cfc706affeed
ozzyozbourne <khanosaid726@gmail.com>
2025-08-11 02:32:45 -0500

    tokiogit add .

--- rust/tk/src/main.rs
+++ rust/tk/src/main.rs
@@ -9,27 +9,48 @@ async fn main() {
         .init();
 
     info!(location = "Main Entry");
-
-    heavy_work().await;
+    let s = tokio::task::spawn_blocking(|| {
+        info!(location = "BLOCKING");
+        sleep(Duration::from_secs(5));
+        info!(location = "UNBLOCKING");
+    });
+    tokio::task::yield_now().await;
     light_work().await;
-
+    _ = s.await;
     info!(location = "Main Exit");
 }
 
 #[instrument]
-async fn heavy_work() {
-    info!(location = "HV Entry");
-
-    _ = tokio::task::spawn_blocking(|| {
-        info!(location = "BLOCKING");
-        sleep(Duration::from_secs(5));
-    })
-    .await;
-
-    info!(location = "HV Exit");
+async fn light_work() {
+    info!(location = "LIGHT - Main");
+    light_work1().await;
 }
 
 #[instrument]
-async fn light_work() {
-    info!(location = "LIGHT");
+async fn light_work1() {
+    info!(location = "LIGHT - 1");
+    light_work2().await;
 }
+
+#[instrument]
+async fn light_work2() {
+    info!(location = "LIGHT - 2");
+    light_work3().await;
+}
+
+#[instrument]
+async fn light_work3() {
+    info!(location = "LIGHT - 3");
+}
+
+// #[instrument]
+// async fn heavy_work() {
+//     info!(location = "HV Entry");
+//     _ = tokio::task::spawn_blocking(|| {
+//         info!(location = "BLOCKING");
+//         sleep(Duration::from_secs(5));
+//     })
+//     .await;
+//
+//     info!(location = "HV Exit");
+// }
