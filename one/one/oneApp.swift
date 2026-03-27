c2820a4deb8763c72b67985899bc4f07428ded08
ozzyozbourne <khanosaid726@gmail.com>
2026-03-27 03:15:13 -0500

    one

---
+++ one/one/oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneApp.swift
+//  one
+//
+//  Created by osaid khan on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
